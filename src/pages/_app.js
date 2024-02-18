6e2f94c4923c493a0d3d70d4fec35715468ad1ff
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-02-19 01:24:32 +0500

    again live with chat

---
+++ src/pages/_app.js
@@ -0,0 +1,28 @@
+import "@/styles/globals.css";
+import "@/styles/custom.css"
+import Headernew from "../component/Headernew";
+import Footer1 from "../component/Footer1";
+import { SfProDisplay, primary } from "@/configs/fonts";
+import Script from "next/script";
+// import Zendesk, { ZendeskAPI } from "../pages/zendesk";
+// const ZENDESK_KEY = "325da280-f4f0-4c80-997f-ea4de45eb2f1";
+
+export default function App({ Component, pageProps }) {
+
+
+  // const handleLoaded = () => {
+  //   zE('webWidget:on', 'open', function () {
+  //   });
+  // };
+
+  return (
+    <>
+      <main className={`${primary.className} ${SfProDisplay.variable}`}>
+        <Headernew />
+        <Component {...pageProps} />
+        <Footer1 />
+      </main>
+      <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=0b817d63-4ced-4290-829c-a9889a9d1780" />
+    </>
+  );
+}
