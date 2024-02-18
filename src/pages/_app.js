01e192642a4f7078e04ebec47ae3f8614b1893c6
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-02-19 01:35:25 +0500

    chat with time

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/custom.css"
+import React, { useEffect } from 'react';
 import Headernew from "../component/Headernew";
 import Footer1 from "../component/Footer1";
 import { SfProDisplay, primary } from "@/configs/fonts";
@@ -8,7 +9,21 @@ import Script from "next/script";
 // const ZENDESK_KEY = "325da280-f4f0-4c80-997f-ea4de45eb2f1";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Create script element
+    const script = document.createElement('script');
+    script.id = 'ze-snippet';
+    script.src = 'https://static.zdassets.com/ekr/snippet.js?key=0b817d63-4ced-4290-829c-a9889a9d1780';
+    script.async = true;
 
+    // Append script to the body
+    document.body.appendChild(script);
+
+    // Clean up script when component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   // const handleLoaded = () => {
   //   zE('webWidget:on', 'open', function () {
@@ -22,7 +37,7 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
         <Footer1 />
       </main>
-      <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=0b817d63-4ced-4290-829c-a9889a9d1780" />
+      
     </>
   );
 }
