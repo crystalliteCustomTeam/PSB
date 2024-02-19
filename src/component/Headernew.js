1190d58c12572958caa655fed62779f47ab3ff54
alim-2023 <alim@crystallitedigital.com>
2024-02-19 18:10:49 +0500

    headernew.js changes

--- src/component/Headernew.js
+++ src/component/Headernew.js
@@ -34,17 +34,18 @@ const Header = () => {
                 </Link>
               </div>
               <nav className="flex space-x-4 text-[16px]   text-[#1d1d1f] font-medium">
-                <ul id="menu" className={`${menuOpen ? 'transform translate-x-0 md:hidden xs:hidden sm:hidden' : 'flex transition-transform duration-500  ease-in-out translate-x-0 md:translate-x-0 right-0 xs:text-white sm:text-white md:text-white md:bg-[#00c0e4] sm:bg-[#00c0e4] md:z-50 sm:z-50 md:text-[18px] xs:text-[18px] sm:text-[18px] md:block xs:block sm:block xs:fixed sm:fixed md:fixed xs:top-0 sm:top-0 md:top-0 sm:py-16 md:py-16 md:w-[70%] md:h-full xs:w-[320px] xs:h-full sm:w-[300px] sm:h-full xs:right-0 md:right-0 xs:px-10 leading-9 xs:py-16 xs:bg-[#00c0e4] xs:z-50 text-[#1d1d1f] '} flex gap-x-10 text-[#1d1d1f] cursor-default `}>
+                <ul id="menu" className={`${menuOpen ? 'transform translate-x-0 md:hidden xs:hidden sm:hidden' : 'flex transition-transform duration-500  ease-in-out translate-x-0 md:translate-x-0 right-0 xs:text-white sm:text-white md:text-white md:bg-[#00c0e4] sm:bg-[#00c0e4] md:z-50 sm:z-50 md:text-[18px] xs:text-[18px] sm:text-[18px] md:block xs:block sm:block xs:fixed sm:fixed md:fixed xs:top-0 sm:top-0 md:top-0 sm:py-16 md:py-16 md:w-[70%] md:h-full xs:w-[320px] xs:h-full sm:w-[300px] sm:h-full xs:right-0 md:right-0 xs:px-10 leading-9 xs:py-16 xs:bg-[#00c0e4] xs:z-50 text-[#1d1d1f] '} flex gap-x-5 text-[#1d1d1f] cursor-default `}>
                   <li className="xl:hidden lg:hidden sm:z-90 xs:z-90 fixed top-4 right-6">
                     <Link href="javascript:void(0)" className="text-right text-white text-4xl" onClick={toggleMenu}>
                       &times;
                     </Link>
                   </li>
                   <li>
-                    <Link href="/" className={`list text-black md:text-white text-[17px] font-bold`}>Home</Link>
+                    <Link href="/" className={`list text-black md:text-white text-[15px] font-bold`}>Home</Link>
                   </li>
-                  <li className="relative group py-0 hover:text-[#00C0E4] z-50">
-                    <button className="hover:opacity-50 cursor-default text-black md:text-white font-[300]" aria-haspopup="true">Services
+                  <li><Link href="/aboutus" className={`list text-black md:text-white text-[15px] font-[300]`}>About</Link></li>
+                  <li className="relative group py-0 hover:text-[#00C0E4] text-[15px] z-50">
+                    <button className="hover:opacity-50 cursor-default text-black md:text-white font-[300]" aria-haspopup="true">Writing Services
                       <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 ml-2 -mr-1 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" ></path>
                       </svg>
@@ -59,6 +60,7 @@ const Header = () => {
                                 The Suite
                               </p>
                               <ul className="mt-3 text-[15px]">
+                              
                                 <li>
                                   <Link href="/affordable-ghostwriting-services" className="flex justify-start align-middle items-center p-2 -mx-2 rounded-lg hover:bg-gradient-to-br hover:from-indigo-50 hover:to-pink-50 hover:via-blue-50 transition ease-in-out duration-300 text-gray-800 font-semibold hover:text-indigo-600">
 
@@ -162,11 +164,15 @@ const Header = () => {
                       </div>
                     </div>
                   </li>
-                  <li><Link href="/hardcover-book-printing-services" className={`list text-black md:text-white text-[17px] font-[300]`}>Printing</Link></li>
-                  <li><Link href="/aboutus" className={`list text-black md:text-white text-[17px] font-[300]`}>About</Link></li>
-                  <li><Link href="/contact-us" className={`list text-black md:text-white text-[17px] font-[300]`}>Contact Us</Link></li>
+                  <li><Link href="/genre" className={`list text-black md:text-white text-[15px] font-[300]`}>Genre</Link></li>
+                  <li><Link href="/ebook-marketing-services" className={`list text-black md:text-white text-[15px] font-[300]`}>Marketing</Link></li>
+                  <li><Link href="/book-publishing-services" className={`list text-black md:text-white text-[15px] font-[300]`}>Publication</Link></li>
+                  <li><Link href="/hardcover-book-printing-services" className={`list text-black md:text-white text-[15px] font-[300]`}>Printing</Link></li>
+                  <li><Link href="/book-cover-design-services" className={`list text-black md:text-white text-[15px] font-[300]`}>Cover Design</Link></li>
+                  <li><Link href="/professional-book-editing-services" className={`list text-black md:text-white text-[15px] font-[300]`}>Editing</Link></li>
+                  <li><Link href="/contact-us" className={`list text-black md:text-white text-[15px] font-[300]`}>Contact Us</Link></li>
                   <li>
-                    <Link href="tel:800-781-9093" className={`text-[17px] font-secondary text-[#40BEE2] flex items-center gap-x-3 font-[500]`}>
+                    <Link href="tel:800-781-9093" className={`text-[15px] font-secondary text-[#40BEE2] flex items-center gap-x-3 font-[500]`}>
                       <Image src={TelIcon} width={15} height={15} alt='Amazon Book Publishing' />
                       <span className='border-l border-[#40BEE2] pl-[10px]'>800-781-9093</span>
                     </Link>
