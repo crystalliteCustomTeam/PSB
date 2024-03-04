bc0aaa70fba7487becc10ce679688365a0915a29
dev-sikander <sikander@crystallitedigital.com>
2024-03-05 03:18:25 +0500

    lp-one page header done

--- src/component/LP-One/index.js
+++ src/component/LP-One/index.js
@@ -1,4 +1,5 @@
+import Header from "./Header/Header"
 import Contact from "./Contact/Contact"
 import Reviews from "./Reviews/Reviews"
 
-export { Contact, Reviews }
+export { Contact, Reviews, Header }
