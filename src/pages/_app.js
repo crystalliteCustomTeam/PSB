746cb22710d16d5c732a60d582a398d10ec3323e
dev-mr <m.rizwan@crystal-lite.net>
2024-04-25 00:59:14 +0500

    Fix GTM and Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,17 +22,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="icon" href="/images/favicon.svg" />
         <meta name="google-site-verification" content="R28eoLjUYrldzCTVPJyB50E-zTd2cWaogKpJP3ycGFM" />
-        <Script id="chat-script-1">{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WQT66SHZ');`}</Script>
-        <Script id="chat-script-2" src="https://www.googletagmanager.com/gtag/js?id=G-66Q1HF396Z"></Script>
-        <Script id="chat-script-3">{`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-66Q1HF396Z');`}</Script>
       </Head>
       <main className={`${primary.className} ${SfProDisplay.variable}`}>
         {router.pathname == "/lp-one" ? "" : <Headernew />}
@@ -96,6 +85,17 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             `
             }
           </Script>
+          <Script id="chat-script-1">{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WQT66SHZ');`}</Script>
+        <Script id="chat-script-2" src="https://www.googletagmanager.com/gtag/js?id=G-66Q1HF396Z"></Script>
+        <Script id="chat-script-3">{`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-66Q1HF396Z');`}</Script>
           <noscript>
             <a href="https://www.livechat.com/chat-with/17215314/" rel="nofollow">
               Chat with us
