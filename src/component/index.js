d57a37e566f2eae5811d1731309ac5da43f531be
dev-mr <m.rizwan@crystal-lite.net>
2024-07-05 00:18:29 +0500

    Improve Page Speed

--- src/component/index.js
+++ src/component/index.js
@@ -1,12 +1,10 @@
 // Next
 import dynamic from "next/dynamic"
 
-// import MRHeroForm from "./MRHeroForm/MRHeroForm"
-const MRHeroForm = dynamic(() => import('./MRHeroForm/MRHeroForm'))
+import MRHeroForm from "./MRHeroForm/MRHeroForm"
 import MRCTA from "./MRCTA/MRCTA"
 import MRCTAGroup from "./MRCTAGroup/MRCTAGroup"
-// import MRHero from "./MRHero/MRHero"
-const MRHero = dynamic(() => import('./MRHero/MRHero'))
+import MRHero from "./MRHero/MRHero"
 import MRHeroCounter from "./MRHeroCounter/MRHeroCounter"
 import MRContact from "./MRContact/MRContact"
 import MRQuestioners from "./MRQuestioners/MRQuestioners"
@@ -17,6 +15,5 @@ import MRLibarySphere from './MRLibrarySphere/MRLibrarySphere'
 import MRTable from './MRTable/MRTable'
 import MRServices from "./MRServices/MRServices"
 const MRVideoTestimonials = dynamic(() => import('./MRVideoTestimonials/MRVideoTestimonials'))
-const ThirdPartyScript = dynamic(() => import('./ThirdPartyScript'))
 
-export { MRHero, MRCTAGroup, MRCTA, MRHeroForm, MRHeroCounter, MRContact, MRQuestioners, MRContent1, MRContent2, MRTable, MRLibarySphere, MRCards, MRServices, MRVideoTestimonials, ThirdPartyScript }
+export { MRHero, MRCTAGroup, MRCTA, MRHeroForm, MRHeroCounter, MRContact, MRQuestioners, MRContent1, MRContent2, MRTable, MRLibarySphere, MRCards, MRServices, MRVideoTestimonials }
