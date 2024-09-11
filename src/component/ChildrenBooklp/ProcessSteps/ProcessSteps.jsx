6c75fde38eef7223af037a5920087249fe5b1b8a
dev-sikander <sikander@crystallitedigital.com>
2024-09-11 22:58:24 +0500

    children book page fixes done

--- src/component/ChildrenBooklp/ProcessSteps/ProcessSteps.jsx
+++ src/component/ChildrenBooklp/ProcessSteps/ProcessSteps.jsx
@@ -37,9 +37,14 @@ const ProcessSteps = () => {
     const [currentStep, setCurrentStep] = useState(0);
     const totalSteps = processData.length;
 
-    const handlePrev = () => {
-        setCurrentStep(currentStep + 1);
-    };
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentStep(prevStep => (prevStep + 1) % totalSteps);
+        }, 1500);
+
+        return () => clearInterval(interval);
+    }, [totalSteps]);
+
     return (
         <section className='pt-[50px] mr-lg:pt-[80px] pb-[50px] mr-lg:pb-[80px]'>
             <div className="mr-container">
@@ -48,27 +53,45 @@ const ProcessSteps = () => {
                         <h2 className='mr-lg:text-[40px] mr-md:text-[30px] text-[25px] font-semibold leading-normal mb-3 text-center text-black'>Our <span className='text-[#40BEE2] font-normal'>5-Step</span> Process Of Self Book <span className='text-[#40BEE2] font-normal'>Publishing</span> Company!</h2>
                         <p className='mr-md:text-base text-sm font-normal leading-normal text-black text-center mr-lg:w-11/12 mr-xl:w-8/12 mx-auto pb-8 mr-xl:pb-14'>Our seasoned marketing professionals expertly employ advanced, effective techniques to boost your literal presence. Our seasoned marketing professionals expertly employ advanced, effective techniques to boost your Consistency.</p>
                     </div>
-                </div>
+                </div> 
                 <div className="grid grid-cols-2 mr-md:grid-cols-5 justify-center gap-5 pb-4">
                     {processData.map((process, index) => (
-                        <div key={index} onClick={() => handlePrev(index)}>
+                        <div
+                            key={index}
+                            className={`${index === processData.length - 1 ? 'col-span-2 ' : ''} mr-md:col-span-1`}
+                        >
                             <div className="image">
-                                <Image src={process.processImage} alt='Process' className='mx-auto w-6/12 mr-md:w-auto' />
+                                <Image src={process.processImage} alt="Process" className={`${index === processData.length - 1 ? 'w-3/12' : 'w-6/12'} mx-auto mr-md:w-auto`} />
                             </div>
-                            {currentStep < totalSteps - 0 && (
-                                <div className={`progress relative pt-2 mr-md:pt-7 pb-6 mr-md:pb-10 ${index < totalSteps - 1
-                                    ? `${index <= currentStep ? 'before:bg-[#40BEE2]' : 'before:bg-[#EFEFEF]'} mr-md:before:content-[''] mr-md:before:absolute mr-md:before:top-[32px] mr-md:before:right-[-52%] mr-md:before:w-[97%] mr-md:before:h-[5px]`
-                                    : ''
-                                    }`}>
-                                    <div className={`absolute top-[24%] left-0 right-0 mx-auto w-[20px] mr-md:w-[35px] h-[20px] mr-md:h-[35px] rounded-full ${index <= currentStep ? 'bg-transparent' : 'bg-[#EFEFEF]'}`}>
-                                        {index <= currentStep ? <Image src={TickImage} alt='Tick Image' /> :
-                                            <span className={`w-[10px] mr-md:w-[16px] h-[10px] mr-md:h-[16px] mx-auto bg-[#CECECE] rounded-full absolute top-[50%] translate-y-[-50%] left-0 right-0`}></span>
-                                        }
+                            {currentStep < totalSteps && (
+                                <div
+                                    className={`progress relative pt-2 mr-md:pt-7 pb-6 mr-md:pb-10 ${index < totalSteps - 1
+                                            ? `${index <= currentStep ? 'before:bg-[#40BEE2]' : 'before:bg-[#EFEFEF]'
+                                            } mr-md:before:content-[''] mr-md:before:absolute mr-md:before:top-[32px] mr-md:before:right-[-52%] mr-md:before:w-[97%] mr-md:before:h-[5px]`
+                                            : ''
+                                        }`}
+                                >
+                                    <div
+                                        className={`absolute top-[24%] left-0 right-0 mx-auto w-[20px] mr-md:w-[35px] h-[20px] mr-md:h-[35px] rounded-full ${index <= currentStep ? 'bg-transparent' : 'bg-[#EFEFEF]'
+                                            }`}
+                                    >
+                                        {index <= currentStep ? (
+                                            <Image src={TickImage} alt="Tick Image" />
+                                        ) : (
+                                            <span
+                                                className={`w-[10px] mr-md:w-[16px] h-[10px] mr-md:h-[16px] mx-auto bg-[#CECECE] rounded-full absolute top-[50%] translate-y-[-50%] left-0 right-0`}
+                                            ></span>
+                                        )}
                                     </div>
                                 </div>
                             )}
                             <div className="txt">
-                                <p className={`text-[16px] mr-xl:text-[20px] leading-[20px] mr-xl:leading-[30px] font-medium text-center ${index <= currentStep ? 'text-black' : 'text-gray-500'}`}>{process.title}</p>
+                                <p
+                                    className={`text-[16px] mr-xl:text-[20px] leading-[20px] mr-xl:leading-[30px] font-medium text-center ${index <= currentStep ? 'text-black' : 'text-gray-500'
+                                        }`}
+                                >
+                                    {process.title}
+                                </p>
                             </div>
                         </div>
                     ))}
