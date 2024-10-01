d7bee17f722243eda9abac07218f11869eab5c82
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-09-30 17:56:18 -0700

    blog inner page designed

--- src/component/MRBlogs/BlogArticles/BlogArticle.jsx
+++ src/component/MRBlogs/BlogArticles/BlogArticle.jsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import { Col, Container, Row } from 'react-bootstrap'
+import Image from "next/image"
 import styles from "./InnerBanner.module.css"
 // Images
 import Arrow from "media/blog-new/arrowBlack.png"
@@ -12,106 +11,106 @@ const BlogArticle = () => {
     return (
         <>
             <section className={styles.readSec}>
-                <Container>
-                    <Row>
-                        <Col lg={12}>
-                            <h2 className='fontsfregular mb-5 text-center'>Read Next</h2>
-                        </Col>
-                        <Col md={6} lg={4} className='p-2'>
+                <div className="mr-container">
+                    <div className="grid grid-cols-12 gap-5">
+                        <div className="col-span-12">
+                            <h2 className="fontsfregular mb-5 text-center">Read Next</h2>
+                        </div>
+                        <div className="mr-xl:col-span-4 mr-md:col-span-6 col-span-12 p-2">
                             <div className={styles.card}>
                                 <div className={styles.cardImg}>
-                                    <Image src={BlogImage1} alt='Bitswits' className='img-fluid w-100' />
+                                    <Image src={BlogImage1} alt="bestsellingpublisher" className="img-fluid w-full" />
                                 </div>
                                 <div className={styles.cardContent}>
-                                    <p className='fontsfregular'>Blockchain</p>
-                                    <h5 className='fontsfregular'>Conquer Tech With Bitswits: <br />
+                                    <p className="fontsfregular">Blockchain</p>
+                                    <h5 className="fontsfregular">Conquer Tech With Bitswits: <br />
                                         Your Path To Unstoppable Progress!</h5>
-                                    <p className='fontsfregular'>Bitswits' Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional. Their Comprehensive Work, From Requirement Analysis</p>
+                                    <p className="fontsfregular">Bitswits" Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional. Their Comprehensive Work, From Requirement Analysis</p>
                                     <div className={styles.btn}>
-                                        <a href="#href" className='text-black'>
-                                            Read More
-                                            <Image src={Arrow} alt='Bitswits' className='ms-2' />
+                                        <a href="#href" className="text-black flex items-center gap-2">
+                                            <span className="block">Read More</span>
+                                            <Image src={Arrow} alt="bestsellingpublisher" className="block" />
                                         </a>
                                     </div>
                                 </div>
                                 <div className={styles.cardBottom}>
                                     <div className={styles.cardBottomImg}>
-                                        <Image src={profile} width={50} height={50} alt='Bitswits' className='img-fluid' />
+                                        <Image src={profile} width={50} height={50} alt="bestsellingpublisher" className="img-fluid" />
                                     </div>
                                     <div className={styles.cardbottomContent}>
-                                        <div className='d-flex align-items-center gap-2 pb-1'>
-                                            <h3 className='fontsfregular'>Nick Willford</h3>
-                                            <Image src={verifies} alt='bitswits' width={13} height={13} />
+                                        <div className="flex items-center gap-2 pb-1">
+                                            <h3 className="fontsfregular">Nick Willford</h3>
+                                            <Image src={verifies} alt="bestsellingpublisher" width={13} height={13} />
                                         </div>
-                                        <p className='fontsfregular'>Bitswits' Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional.</p>
+                                        <p className="fontsfregular">Bitswits" Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional.</p>
                                     </div>
                                 </div>
                             </div>
-                        </Col>
-                        <Col md={6} lg={4} className='p-2'>
+                        </div>
+                        <div className="mr-xl:col-span-4 mr-md:col-span-6 col-span-12 p-2">
                             <div className={styles.card}>
                                 <div className={styles.cardImg}>
-                                    <Image src={BlogImage2} alt='Bitswits' className='img-fluid w-100' />
+                                    <Image src={BlogImage2} alt="bestsellingpublisher" className="img-fluid w-full" />
                                 </div>
                                 <div className={styles.cardContent}>
-                                    <p className='fontsfregular'>Mobile Application</p>
-                                    <h5 className='fontsfregular'>Conquer Tech With Bitswits: <br />
+                                    <p className="fontsfregular">Mobile Application</p>
+                                    <h5 className="fontsfregular">Conquer Tech With Bitswits: <br />
                                         Your Path To Unstoppable Progress!</h5>
-                                    <p className='fontsfregular'>Bitswits' Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional. Their Comprehensive Work, From Requirement Analysis</p>
+                                    <p className="fontsfregular">Bitswits" Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional. Their Comprehensive Work, From Requirement Analysis</p>
                                     <div className={styles.btn}>
-                                        <a href="#href" className='text-black'>
-                                            Read More
-                                            <Image src={Arrow} alt='Bitswits' className='ms-2' />
+                                        <a href="#href" className="text-black flex items-center gap-2">
+                                            <span className="block">Read More</span>
+                                            <Image src={Arrow} alt="bestsellingpublisher" className="block" />
                                         </a>
                                     </div>
                                 </div>
                                 <div className={styles.cardBottom}>
                                     <div className={styles.cardBottomImg}>
-                                        <Image src={profile} width={50} height={50} alt='Bitswits' className='img-fluid' />
+                                        <Image src={profile} width={50} height={50} alt="bestsellingpublisher" className="img-fluid" />
                                     </div>
                                     <div className={styles.cardbottomContent}>
-                                        <div className='d-flex align-items-center gap-2 pb-1'>
-                                            <h3 className='fontsfregular'>Nick Willford</h3>
-                                            <Image src={verifies} alt='bitswits' width={13} height={13} />
+                                        <div className="flex items-center gap-2 pb-1">
+                                            <h3 className="fontsfregular">Nick Willford</h3>
+                                            <Image src={verifies} alt="bestsellingpublisher" width={13} height={13} />
                                         </div>
-                                        <p className='fontsfregular'>Bitswits' Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional.</p>
+                                        <p className="fontsfregular">Bitswits" Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional.</p>
                                     </div>
                                 </div>
                             </div>
-                        </Col>
-                        <Col lg={4} className='p-2'>
+                        </div>
+                        <div className="mr-xl:col-span-4 mr-md:col-span-6 col-span-12 p-2">
                             <div className={styles.card}>
                                 <div className={styles.cardImg}>
-                                    <Image src={BlogImage3} alt='Bitswits' className='img-fluid w-100' />
+                                    <Image src={BlogImage3} alt="bestsellingpublisher" className="img-fluid w-full" />
                                 </div>
                                 <div className={styles.cardContent}>
-                                    <p className='fontsfregular'>General</p>
-                                    <h5 className='fontsfregular'>Conquer Tech With Bitswits: <br />
+                                    <p className="fontsfregular">General</p>
+                                    <h5 className="fontsfregular">Conquer Tech With Bitswits: <br />
                                         Your Path To Unstoppable Progress!</h5>
-                                    <p className='fontsfregular'>Bitswits' Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional. Their Comprehensive Work, From Requirement Analysis</p>
+                                    <p className="fontsfregular">Bitswits" Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional. Their Comprehensive Work, From Requirement Analysis</p>
                                     <div className={styles.btn}>
-                                        <a href="#href" className='text-black'>
-                                            Read More
-                                            <Image src={Arrow} alt='Bitswits' className='ms-2' />
+                                        <a href="#href" className="text-black flex items-center gap-2">
+                                            <span className="block">Read More</span>
+                                            <Image src={Arrow} alt="bestsellingpublisher" className="block" />
                                         </a>
                                     </div>
                                 </div>
                                 <div className={styles.cardBottom}>
                                     <div className={styles.cardBottomImg}>
-                                        <Image src={profile} width={50} height={50} alt='Bitswits' className='img-fluid' />
+                                        <Image src={profile} width={50} height={50} alt="bestsellingpublisher" className="img-fluid" />
                                     </div>
                                     <div className={styles.cardbottomContent}>
-                                        <div className='d-flex align-items-center gap-2 pb-1'>
-                                            <h3 className='fontsfregular'>Nick Willford</h3>
-                                            <Image src={verifies} alt='bitswits' width={13} height={13} />
+                                        <div className="flex items-center gap-2 pb-1">
+                                            <h3 className="fontsfregular">Nick Willford</h3>
+                                            <Image src={verifies} alt="bestsellingpublisher" width={13} height={13} />
                                         </div>
-                                        <p className='fontsfregular'>Bitswits' Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional.</p>
+                                        <p className="fontsfregular">Bitswits" Development Of The Proeye Project, A Wallet Analytics And Search Engine Platform On Blockchain, Was Exceptional.</p>
                                     </div>
                                 </div>
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
--- src/component/MRBlogs/BlogNavigation/BlogNavigation.jsx
+++ src/component/MRBlogs/BlogNavigation/BlogNavigation.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
 // ===== CSS
-import styles from './Blognavigations.module.css';
+import styles from "./Blognavigations.module.css";
 // ===== Images
-import { FaAngleDown } from 'react-icons/fa';
+import { FaAngleDown } from "react-icons/fa";
 const BlogNavigation = ({ postData }) => {
     const [h2Tags, setH2Tags] = useState([]);
     const [activeHeading, setActiveHeading] = useState(null);
@@ -12,11 +12,11 @@ const BlogNavigation = ({ postData }) => {
     useEffect(() => {
         if (postData && postData.content) {
             const parser = new DOMParser();
-            const doc = parser.parseFromString(postData.content, 'text/html');
-            const h2Elements = doc.querySelectorAll('h2');
+            const doc = parser.parseFromString(postData.content, "text/html");
+            const h2Elements = doc.querySelectorAll("h2");
             // h2Elements.forEach((h2, index) => {
             //     const id = `heading${index + 1}`;
-            //     h2.setAttribute('id', id);
+            //     h2.setAttribute("id", id);
             // });
             let h2ElementLeft = document.querySelectorAll("#leftcontent h2");
             Array.from(h2ElementLeft).map((e, i) => {
@@ -25,7 +25,7 @@ const BlogNavigation = ({ postData }) => {
             const h2TagsArray = Array.from(h2Elements).map(h2 => h2.innerText);
             setH2Tags(h2TagsArray);
             if (h2Elements.length > 0) {
-                const firstHeadingId = `#${h2Elements[0].getAttribute('id')}`;
+                const firstHeadingId = `#${h2Elements[0].getAttribute("id")}`;
                 setActiveHeading(firstHeadingId);
             }
         }
@@ -42,57 +42,53 @@ const BlogNavigation = ({ postData }) => {
     return (
         <>
             <div className={styles.sidebar}>
-                <Row>
-                    <Col lg={12}>
-                        <div className={styles.navigations}>
-                            <div className={styles.mainHeading} onClick={toggleNavVisibility}>
-                                <h3 className='fontsfregular'>
-                                    Navigation
-                                    <FaAngleDown className='ms-3' />
-                                </h3>
-                            </div>
-                            {isNavVisible && (
-                                <ul className={styles.navHeadings}>
-                                    {h2Tags.map((text, index) => (
-                                        <li key={index}>
-                                            <a
-                                                href={`#${'heading' + (index + 1)}`}
-                                                className={`fontsfregular ${activeHeading === `#heading${index + 1}` ? styles.active : 'text-black'}`}
-                                                onClick={() => handleHeadingClick(`#heading${index + 1}`)}
-                                            >
-                                                {text}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </div>
-                        <div className={styles.articles}>
-                            <h3 className='fontsfregular'>Relevant Articles</h3>
-                            <ul className={`${styles.noList} mt-4`}>
-                                <li>
-                                    <p className='fontsfregular'>Blockchain</p>
-                                    <h6 className='fontsfregular'>Conquer Tech With Bitswits: <br />
-                                        Your Path To Unstoppable Progress!</h6>
+                <div className={styles.navigations}>
+                    <div className={styles.mainHeading} onClick={toggleNavVisibility}>
+                        <h3 className="flex justify-center items-center gap-2">
+                            <span className="block">Navigation</span>
+                            <FaAngleDown />
+                        </h3>
+                    </div>
+                    {isNavVisible && (
+                        <ul className={styles.navHeadings}>
+                            {h2Tags.map((text, i) => (
+                                <li key={i}>
+                                    <a
+                                        href={`#${"heading" + (i + 1)}`}
+                                        className={`fontsfregular ${activeHeading === `#heading${i + 1}` ? styles.active : "text-black"}`}
+                                        onClick={() => handleHeadingClick(`#heading${i + 1}`)}
+                                    >
+                                        {text}
+                                    </a>
                                 </li>
-                                <li>
-                                    <p className='fontsfregular'>Mobile Application</p>
-                                    <h6 className='fontsfregular'>Frequently Asked Questions <br />Regarding The Development Of <br />Cryptocurrency Exchange Software</h6>
-                                </li>
-                                <li>
-                                    <p className='fontsfregular'>General</p>
-                                    <h6 className='fontsfregular'>Conquer Tech With Bitswits: <br />
-                                        Your Path To Unstoppable Progress!</h6>
-                                </li>
-                                <li>
-                                    <p className='fontsfregular'>Mobile Application</p>
-                                    <h6 className='fontsfregular'>Conquer Tech With Bitswits: <br />
-                                        Your Path To Unstoppable Progress!</h6>
-                                </li>
-                            </ul>
-                        </div>
-                    </Col>
-                </Row>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+                <div className={`${styles.articles} mr-lg:block hidden`}>
+                    <h3 className="fontsfregular">Relevant Articles</h3>
+                    <ul className={`${styles.noList} mt-4`}>
+                        <li>
+                            <p className="fontsfregular">Blockchain</p>
+                            <h6 className="fontsfregular">Conquer Tech With Bitswits: <br />
+                                Your Path To Unstoppable Progress!</h6>
+                        </li>
+                        <li>
+                            <p className="fontsfregular">Mobile Application</p>
+                            <h6 className="fontsfregular">Frequently Asked Questions <br />Regarding The Development Of <br />Cryptocurrency Exchange Software</h6>
+                        </li>
+                        <li>
+                            <p className="fontsfregular">General</p>
+                            <h6 className="fontsfregular">Conquer Tech With Bitswits: <br />
+                                Your Path To Unstoppable Progress!</h6>
+                        </li>
+                        <li>
+                            <p className="fontsfregular">Mobile Application</p>
+                            <h6 className="fontsfregular">Conquer Tech With Bitswits: <br />
+                                Your Path To Unstoppable Progress!</h6>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     );
