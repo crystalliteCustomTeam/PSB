bfafc420b00ab2813b03f7f0e5a8a8d96552c283
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2024-12-07 04:13:17 +0500

    footer changes update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,55 +10,62 @@ import { useRouter } from "next/router"
 import Loader from "@/component/Loader/Loader"
 
 export default function App({ Component, pageProps }) {
-  const [loadScript, setLoadScript] = useState(false);
+  const [loadScript, setLoadScript] = useState(false)
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoadScript(true), 5000);
-    return () => clearTimeout(timer);
-  }, []);
-  const router = useRouter();
+    const timer = setTimeout(() => setLoadScript(true), 5000)
+    return () => clearTimeout(timer)
+  }, [])
+  const router = useRouter()
   return (
     <>
       <Head>
         <link rel="icon" href="/images/favicon.svg" />
-        <meta name="google-site-verification" content="R28eoLjUYrldzCTVPJyB50E-zTd2cWaogKpJP3ycGFM" />
+        <meta
+          name="google-site-verification"
+          content="R28eoLjUYrldzCTVPJyB50E-zTd2cWaogKpJP3ycGFM"
+        />
       </Head>
       <main className={`${primary.className} ${SfProDisplay.variable}`}>
-        {router.pathname == "/lp-one"
-          || router.pathname == "/audiobook-production-services"
-          || router.pathname == "/book-marketing-company"
-          || router.pathname == "/case-studies" || router.pathname == "/book-illustration-services"
-          || router.pathname == "/children-book-publishing-lp"
-          || router.pathname == "/lp/book-publishing-company"
-          || router.pathname == "/blogs"
-          || router.pathname == "/lp/book-marketing-company"
-          || router.pathname == "/booknewbrand-v2"
-          ? "" : <Headernew />}
+        {router.pathname == "/lp-one" ||
+        router.pathname == "/audiobook-production-services" ||
+        router.pathname == "/book-marketing-company" ||
+        router.pathname == "/case-studies" ||
+        router.pathname == "/book-illustration-services" ||
+        router.pathname == "/children-book-publishing-lp" ||
+        router.pathname == "/lp/book-publishing-company" ||
+        router.pathname == "/blogs" ||
+        router.pathname == "/lp/book-marketing-company" ||
+        router.pathname == "/booknewbrand-v2" ? (
+          ""
+        ) : (
+          <Headernew />
+        )}
         {/* {loadScript ? <Component {...pageProps} /> : <Loader />} */}
         <Component {...pageProps} />
-        {router.pathname == "/book-marketing-company"
-          || router.pathname == "/audiobook-production-services"
-          || router.pathname == "/lp/book-publishing-company"
-          || router.pathname == "/book-illustration-services"
-          || router.pathname == "/children-book-publishing-lp"
-          || router.pathname == "/lp/book-marketing-company"
-          || router.pathname == "/booknewbrand-v2"
-          ? "" : <Footer1 />}
+        {router.pathname == "/book-marketing-company" ||
+        router.pathname == "/audiobook-production-services" ||
+        router.pathname == "/lp/book-publishing-company" ||
+        router.pathname == "/book-illustration-services" ||
+        router.pathname == "/children-book-publishing-lp" ||
+        router.pathname == "/lp/book-marketing-company" ||
+        router.pathname == "/booknewbrand-v2" ? (
+          ""
+        ) : (
+          <Footer1 />
+        )}
       </main>
       {loadScript && (
         <>
           <Script>
-            {
-              `
+            {`
               if (window.LiveChatWidget) {
                 window.LiveChatWidget.call('maximize');
               }
-            `
-            }
+            `}
           </Script>
           <Script id="chat-script">
-            {
-              `
+            {`
             window.__lc = window.__lc || {};
             window.__lc.license = 17215314;
         
@@ -98,26 +105,35 @@ export default function App({ Component, pageProps }) {
               !n.__lc.asyncInit && e.init();
               n.LiveChatWidget = n.LiveChatWidget || e;
             })(window, document, [].slice);
-            `
-            }
+            `}
           </Script>
           <Script id="chat-script-1">{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WQT66SHZ');`}</Script>
-          <Script id="chat-script-2" src="https://www.googletagmanager.com/gtag/js?id=G-66Q1HF396Z"></Script>
+          <Script
+            id="chat-script-2"
+            src="https://www.googletagmanager.com/gtag/js?id=G-66Q1HF396Z"
+          ></Script>
           <Script id="chat-script-3">{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-66Q1HF396Z');`}</Script>
           <noscript>
-            <a href="https://www.livechat.com/chat-with/17215314/" rel="nofollow">
+            <a
+              href="https://www.livechat.com/chat-with/17215314/"
+              rel="nofollow"
+            >
               Chat with us
             </a>
-            , powered by{' '}
-            <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">
+            , powered by{" "}
+            <a
+              href="https://www.livechat.com/?welcome"
+              rel="noopener nofollow"
+              target="_blank"
+            >
               LiveChat
             </a>
           </noscript>
