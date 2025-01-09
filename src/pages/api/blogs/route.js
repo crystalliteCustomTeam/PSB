8fcf98a7b88600c0a7ad5a7d2ee77c5995563c09
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-01-09 22:35:31 +0500

    remove blogs page

--- src/pages/api/blogs/route.js
+++ src/pages/api/blogs/route.js
@@ -1,124 +1,124 @@
-const fetchAPI = async (query) => {
-    try {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_GRAPH_ENDPOINT}?query=${encodeURIComponent(query)}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            }
-        );
+// const fetchAPI = async (query) => {
+//     try {
+//         const res = await fetch(
+//             `${process.env.NEXT_PUBLIC_GRAPH_ENDPOINT}?query=${encodeURIComponent(query)}`,
+//             {
+//                 method: "GET",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                 }
+//             }
+//         );
 
-        if (!res.ok) {
-            console.error('Network response was not ok:', res.statusText);
-            throw new Error(`Network response was not ok: ${res.statusText}`);
-        }
+//         if (!res.ok) {
+//             console.error('Network response was not ok:', res.statusText);
+//             throw new Error(`Network response was not ok: ${res.statusText}`);
+//         }
 
-        const json = await res.json();
+//         const json = await res.json();
 
-        if (json.errors) {
-            console.error('API returned errors:', json.errors);
-            throw new Error('Failed to fetch API: ' + JSON.stringify(json.errors));
-        }
+//         if (json.errors) {
+//             console.error('API returned errors:', json.errors);
+//             throw new Error('Failed to fetch API: ' + JSON.stringify(json.errors));
+//         }
 
-        return json.data;
-    } catch (error) {
-        console.error('Error fetching API:', error);
-        throw new Error('API request failed: ' + error.message);
-    }
-};
+//         return json.data;
+//     } catch (error) {
+//         console.error('Error fetching API:', error);
+//         throw new Error('API request failed: ' + error.message);
+//     }
+// };
 
-export const getPostList = async (endCursor = null, taxonomy = null) => {
-    let condition = `after: "${endCursor}", first: 6, where: {orderby: {field: DATE, order: DESC}}`;
+// export const getPostList = async (endCursor = null, taxonomy = null) => {
+//     let condition = `after: "${endCursor}", first: 6, where: {orderby: {field: DATE, order: DESC}}`;
 
-    if (taxonomy) {
-        condition += `, ${taxonomy.key}: "${taxonomy.value}"`;
-    }
+//     if (taxonomy) {
+//         condition += `, ${taxonomy.key}: "${taxonomy.value}"`;
+//     }
 
-    const query = `{
-        bsps {
-            nodes {
-                date
-                slug
-                title
-                excerpt(format: RENDERED)
-                featuredImage {
-                    node {
-                        mediaDetails {
-                            file
-                            sizes {
-                                sourceUrl
-                                width
-                                height
-                            }
-                        }
-                    }
-                }
-                author {
-                    node {
-                        name
-                        avatar {
-                            url
-                        }
-                        description
-                    }
-                }
-                bSPCategories {
-                    nodes {
-                        name
-                        slug
-                    }
-                }
-            }
-        }
-    }`;
+//     const query = `{
+//         bsps {
+//             nodes {
+//                 date
+//                 slug
+//                 title
+//                 excerpt(format: RENDERED)
+//                 featuredImage {
+//                     node {
+//                         mediaDetails {
+//                             file
+//                             sizes {
+//                                 sourceUrl
+//                                 width
+//                                 height
+//                             }
+//                         }
+//                     }
+//                 }
+//                 author {
+//                     node {
+//                         name
+//                         avatar {
+//                             url
+//                         }
+//                         description
+//                     }
+//                 }
+//                 bSPCategories {
+//                     nodes {
+//                         name
+//                         slug
+//                     }
+//                 }
+//             }
+//         }
+//     }`;
 
-    const data = await fetchAPI(query);
-    return data.bsps.nodes;
+//     const data = await fetchAPI(query);
+//     return data.bsps.nodes;
 
-};
+// };
 
-export const getSinglePost = async (slug = "diy-vs-hiring-book-cover-designers-deciding-whats-best") => {
-    const query = `{
-        bspBy(slug: "${slug}") {
-            content(format: RENDERED)
-            date
-            excerpt(format: RENDERED)
-            modified
-            slug
-            title(format: RENDERED)
-            databaseId
-            featuredImage {
-                node {
-                    mediaDetails {
-                        file
-                        sizes {
-                            sourceUrl
-                            width
-                            height
-                        }
-                    }
-                }
-            }
-            author {
-                node {
-                    name
-                    avatar {
-                        url
-                    }
-                    description
-                }
-            }
-            bSPCategories {
-                nodes {
-                    name
-                    slug
-                }
-            }
-        }
-    }`;
+// export const getSinglePost = async (slug = "diy-vs-hiring-book-cover-designers-deciding-whats-best") => {
+//     const query = `{
+//         bspBy(slug: "${slug}") {
+//             content(format: RENDERED)
+//             date
+//             excerpt(format: RENDERED)
+//             modified
+//             slug
+//             title(format: RENDERED)
+//             databaseId
+//             featuredImage {
+//                 node {
+//                     mediaDetails {
+//                         file
+//                         sizes {
+//                             sourceUrl
+//                             width
+//                             height
+//                         }
+//                     }
+//                 }
+//             }
+//             author {
+//                 node {
+//                     name
+//                     avatar {
+//                         url
+//                     }
+//                     description
+//                 }
+//             }
+//             bSPCategories {
+//                 nodes {
+//                     name
+//                     slug
+//                 }
+//             }
+//         }
+//     }`;
 
-    const data = await fetchAPI(query);
-    return data.bspBy
-};
+//     const data = await fetchAPI(query);
+//     return data.bspBy
+// };
