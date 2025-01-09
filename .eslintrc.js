c943d56f5a07d7ccefd94d4ebb87ecac22121125
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-01-09 23:02:48 +0500

    remove blogs page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,37 @@
 module.exports = {
-    parser: 'babel-eslint',
-    "settings": {
-        "react": {
-            "version": "detect"
-        },
-        "env": {
-            "browser": true,
-            "es2021": true,
-        },
-        "extends": [
-            "eslint:recommended",
-            "plugin:react/recommended",
-            "next",
-            "next/core-web-vitals",
-        ],
-        "overrides": [
-            {
-                "env": {
-                    "node": true
-                },
-                "files": [
-                    ".eslintrc.{js,cjs}"
-                ],
-                "parserOptions": {
-                    "sourceType": "script"
-                },
-            },
-        ],
-        "parserOptions": {
-            "ecmaVersion": "latest",
-            "sourceType": "module",
-            
-        },
-        "plugins": [
-            "react",
-            
-        ],
-        "rules": {
-            "react/no-unescaped-entities": 0,
-        },
-    }
+  parser: "@babel/eslint-parser",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "next",
+    "next/core-web-vitals",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/no-unescaped-entities": 0,
+  },
 }
