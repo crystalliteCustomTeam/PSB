7fb4545628bafc12486804031255f23902961d77
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-01-10 00:29:31 +0500

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,38 @@
 module.exports = {
-    parser: 'babel-eslint',
-    "settings": {
-        "react": {
-            "version": "detect"
+  parser: "babel-eslint",
+  settings: {
+    react: {
+      version: "detect",
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "next",
+      "next/core-web-vitals",
+    ],
+    overrides: [
+      {
+        env: {
+          node: true,
         },
-        "env": {
-            "browser": true,
-            "es2021": true,
+        files: [".eslintrc.{js,cjs}"],
+        parserOptions: {
+          sourceType: "script",
         },
-        "extends": [
-            "eslint:recommended",
-            "plugin:react/recommended",
-            "next",
-            "next/core-web-vitals",
-        ],
-        "overrides": [
-            {
-                "env": {
-                    "node": true
-                },
-                "files": [
-                    ".eslintrc.{js,cjs}"
-                ],
-                "parserOptions": {
-                    "sourceType": "script"
-                },
-            },
-        ],
-        "parserOptions": {
-            "ecmaVersion": "latest",
-            "sourceType": "module",
-            
-        },
-        "plugins": [
-            "react",
-            
-        ],
-        "rules": {
-            "react/no-unescaped-entities": 0,
-        },
-    }
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  },
 }
