548c4abf4d5dc344c40585cdf0844bc4e8a67425
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-06-19 21:05:05 +0500

    first commit

--- src/component/Lp-Seven-2/Header.jsx
+++ src/component/Lp-Seven-2/Header.jsx
@@ -107,7 +107,7 @@ const Header = () => {
             <div className="mr-md:flex hidden items-center gap-x-5">
               <CTA handle="parent.LC_API.open_chat_window();return false;" />
               <CTA
-                text="+855-666-6675"
+                text="855-666-6675"
                 link="tel:8556666675"
                 variant="secondary"
                 svgClasses="border border-black rounded-full"
